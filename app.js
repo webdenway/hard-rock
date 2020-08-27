7235f4496004e473a253957cd622046b7c3608c6
webdenway <clthbbd@gmail.com>
2020-08-27 17:10:56 +0600

    update

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const searchButton = document.getElementById('search-button');
 
 
 searchButton.addEventListener('click', function() {
-    const inputField = document.getElementById('input-lyrics');
+    let inputField = document.getElementById('input-lyrics');
 
     fetch(`${"https://api.lyrics.ovh/suggest/"}${inputField.value}`)
         .then(response => response.json())
         .then(json => albumDetails(json))
 
     function albumDetails(json) {
-        console.log(json);
+        // console.log(json);
 
         for (let i = 0; i < json.data.length - 5; i++) {
 
             const albumTitle = json.data[i].album.title;
-            const artistMan = json.data[i].artist.name;
-            const songName = json.data[i].title;
-            console.log(artistMan);
+            let artistMan = json.data[i].artist.name;
+            let songName = json.data[i].title;
             detail = document.getElementById('output');
 
             const displayResult = `<div class="single-result row align-items-center my-3 p-3">
@@ -26,13 +25,35 @@ searchButton.addEventListener('click', function() {
                 <p class="author lead">Album by <span>${albumTitle}</span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button class="btn btn-success" onClick = 'myFunction("${artistMan}","${songName}")'>Get Lyrics</button>
             </div>
         </div>`
 
             detail.innerHTML += displayResult;
+
         }
+
 
     }
 
+
 })
+
+
+
+function myFunction(artistMan, songName) {
+
+
+    fetch(`https://api.lyrics.ovh/v1/${artistMan}/${songName}`)
+        .then(res => res.json())
+        .then(data => findLyrics(data))
+
+    function findLyrics(data) {
+        if (data.lyrics == 'data.error' || data.lyrics == 'data.undefined') {
+            document.getElementById('show-lyric').innerHTML = `<p> No Lyrics Found </p>`
+        } else {
+            document.getElementById('show-lyric').innerHTML = `${data.lyrics}`
+        }
+        console.log(data);
+    }
+}
